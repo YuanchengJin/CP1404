4c17223e986160579657be0b065712934a6ffcb0
Yuancheng_Jin <yuancheng.jin@my.jcu.edu.au>
2024-07-21 21:18:00 +0800

    prac5 word occurrences

---
+++ practicals/prac_07/guitar.py
@@ -0,0 +1,23 @@
+class Guitar:
+    def __init__(self, name="", year=0, cost=0):
+        """Initialise a Guitar instance."""
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        """Return a string representation of the guitar."""
+        return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
+
+    def get_age(self):
+        """Return the age of the guitar in years."""
+        CURRENT_YEAR = 2024
+        return CURRENT_YEAR - self.year
+
+    def is_vintage(self):
+        """Return True if the guitar is 50 or more years old, False otherwise."""
+        return self.get_age() >= 50
+
+    def __lt__(self, other):
+        """Less than, used for sorting Guitars by year."""
+        return self.year < other.year
